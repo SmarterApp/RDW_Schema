9462da7055e03d55b6cb8f9fc59257eebd072d86
Rob Cordeau <rcordeau@fairwaytech.com>
2017-05-18 10:08:30 -0700

    updated for translation table key constraint

--- warehouse/sql/V201702061486427077__initial_ddl.sql
+++ warehouse/sql/V201702061486427077__initial_ddl.sql
@@ -250,6 +250,7 @@ CREATE TABLE IF NOT EXISTS accommodation_translation (
   accommodation_id smallint NOT NULL,
   language_id tinyint NOT NULL,
   label varchar(40) NOT NULL,
+  CONSTRAINT uk__accommodation_id__language_id UNIQUE KEY (accommodation_id, language_id),
   CONSTRAINT fk__accommodation_translation__accommodation FOREIGN KEY (accommodation_id) REFERENCES accommodation(id),
   CONSTRAINT fk__accommodation_translation__language FOREIGN KEY (language_id) REFERENCES language(id)
 );
