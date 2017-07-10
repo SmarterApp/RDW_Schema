1f5370779e956d3ec55f5041007ae87332674e26
Rob Cordeau <rcordeau@fairwaytech.com>
2017-07-10 23:35:11 +0200

    dropping changes to label_code column size

--- reporting/sql/V201707071499469491__translation_types.sql
+++ reporting/sql/V201707071499469491__translation_types.sql
@@ -2,8 +2,5 @@
 
 USE ${schemaName};
 
-#Increasing the size of the columns, but need to shrink the size of the prefix of lable_code that gets indexed
-ALTER TABLE translation MODIFY label text, 
-	MODIFY label_code varchar(256), 
-	DROP INDEX idx__translation__namespace_label_code_language_code,
-	ADD UNIQUE INDEX idx__translation__namespace_label_code_language_code (namespace, label_code(241), language_code);
+# Changing the type of the label column from varchar to text
+ALTER TABLE translation MODIFY label text;
