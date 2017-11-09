3685caec5f6b0ee47c27bfc81c3fb6ddef28d84f
Alla Gorina <agorina@fairwaytech.com>
2017-11-08 18:50:56 -0800

    DWR-1104  Modified to not update a student if the given date is older than the stored date

--- warehouse/sql/V1_1_0_19__student_upsert_fix_inferred_school.sql
+++ warehouse/sql/V1_1_0_19__student_upsert_fix_inferred_school.sql
@@ -1,9 +1,13 @@
-USE ${schemaName};
 
-/************************************* Stored procedures ***************************************/
+USE ${schemaName};
 
 DROP PROCEDURE IF EXISTS student_upsert;
 
+-- Modified to not update a student if p_inferred_effective_date is before the currently stored date.
+-- The caller can use p_update to determine what action has been performed:
+-- p_update = 0 - nothing has been touched, either nothing has changed or the p_inferred_effective_date is older than the one stored
+-- p_update = 1 - a record has been updated
+-- p_update = 2 - a new record has been created
 DELIMITER //
 CREATE PROCEDURE student_upsert(IN  p_ssid                          VARCHAR(65),
                                 IN  p_last_or_surname               VARCHAR(60),
@@ -26,9 +30,15 @@ CREATE PROCEDURE student_upsert(IN  p_ssid                          VARCHAR(65),
       SELECT id, 0 INTO p_id, p_updated FROM student WHERE ssid = p_ssid;
     END;
 
-    SELECT id, 0 INTO p_id, p_updated FROM student WHERE ssid = p_ssid;
+    -- see if it is an existing student and if the data is fresher than the one stored
+    SELECT id,
+            CASE WHEN (p_inferred_effective_date IS NOT NULL AND inferred_effective_date IS NOT NULL AND p_inferred_effective_date < inferred_effective_date)
+                THEN 0
+            ELSE 1 END
+            INTO p_id, p_updated
+        FROM student WHERE ssid = p_ssid;
 
-    IF (p_id IS NOT NULL)
+    IF (p_id IS NOT NULL AND p_updated = 1)
     THEN
        IF(p_inferred_school_id IS NULL)
          THEN
@@ -66,12 +76,13 @@ CREATE PROCEDURE student_upsert(IN  p_ssid                          VARCHAR(65),
           lep_entry_at                  = p_lep_entry_at,
           lep_exit_at                   = p_lep_exit_at,
           birthday                      = p_birthday,
-          inferred_effective_date       = CASE WHEN (p_inferred_effective_date IS NULL OR p_inferred_effective_date >= COALESCE(inferred_effective_date, p_inferred_effective_date)) THEN p_inferred_effective_date ELSE inferred_effective_date END,
-          inferred_school_id            = CASE WHEN (p_inferred_effective_date IS NULL OR p_inferred_effective_date >= COALESCE(inferred_effective_date, p_inferred_effective_date)) THEN p_inferred_school_id ELSE inferred_school_id END,
+          inferred_effective_date       = p_inferred_effective_date,
+          inferred_school_id            = p_inferred_school_id,
           update_import_id              = p_import_id
         WHERE id = p_id;
       END IF;
-    ELSE
+    ELSEIF (p_id IS NULL)
+    THEN
       INSERT INTO student (ssid, last_or_surname, first_name, middle_name, gender_id, first_entry_into_us_school_at, lep_entry_at, lep_exit_at, birthday, inferred_school_id, inferred_effective_date, import_id, update_import_id)
       VALUES (p_ssid, p_last_or_surname, p_first_name, p_middle_name, p_gender_id, p_first_entry_into_us_school_at, p_lep_entry_at, p_lep_exit_at, p_birthday, p_inferred_school_id, p_inferred_effective_date, p_import_id, p_import_id);
 
