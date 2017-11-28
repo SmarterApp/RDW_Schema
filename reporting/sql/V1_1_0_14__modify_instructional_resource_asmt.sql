3fe18a7dd3de4541df8233ec764fddb2b158a83f
wtritch <wtritchler@fairwaytech.com>
2017-11-27 16:21:17 -0800

    Accounting for code review comments.

--- reporting/sql/V1_1_0_14__modify_instructional_resource_asmt.sql
+++ reporting/sql/V1_1_0_14__modify_instructional_resource_asmt.sql
@@ -7,3 +7,6 @@ ALTER TABLE instructional_resource
   DROP INDEX idx__instructional_resource,
   CHANGE asmt_natural_id asmt_name VARCHAR(250) NOT NULL,
   ADD UNIQUE INDEX idx__instructional_resource (asmt_name, org_level, performance_level, org_id);
+
+ALTER TABLE asmt
+  ADD INDEX idx__asmt__name (name);
