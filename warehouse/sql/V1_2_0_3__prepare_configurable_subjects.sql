4a9ededf97b7cb680553aa343d3edbbc80c7d734
wtritch <wtritchler@fairwaytech.com>
2018-04-26 10:14:43 -0700

    Fix nullable declarations

--- warehouse/sql/V1_2_0_3__prepare_configurable_subjects.sql
+++ warehouse/sql/V1_2_0_3__prepare_configurable_subjects.sql
@@ -7,21 +7,21 @@ USE ${schemaName};
 -- Warehouse translation table for holding English message translations provided
 -- with the subject XML
 CREATE TABLE translation (
-  namespace VARCHAR(10),
-  label_code VARCHAR(128),
-  language_code VARCHAR(3),  -- Should this be assumed as "eng" ?
+  namespace VARCHAR(10) NOT NULL,
+  label_code VARCHAR(128) NOT NULL,
+  language_code VARCHAR(3) NOT NULL,  -- Should this be assumed as "eng" ?
   label TEXT,
   PRIMARY KEY(namespace, label_code, language_code)
 );
 
 -- Table for holding subject configurations in the context of an assessment type
 CREATE TABLE subject_asmt_type (
-  asmt_type_id TINYINT,
-  subject_id TINYINT,
-  performance_levels TINYINT,
-  performance_level_standard_cutoff TINYINT NULL,
-  sub_score_performance_levels TINYINT,
-  sub_score_performance_level_standard_cutoff TINYINT NULL,
+  asmt_type_id TINYINT NOT NULL,
+  subject_id TINYINT NOT NULL,
+  performance_level_count TINYINT NOT NULL,
+  performance_level_standard_cutoff TINYINT,
+  sub_score_performance_level_count TINYINT,
+  sub_score_performance_level_standard_cutoff TINYINT,
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY(asmt_type_id, subject_id),
@@ -32,10 +32,10 @@ CREATE TABLE subject_asmt_type (
 -- Table for holding hierarchical item categories (replaces claim and target tables)
 CREATE TABLE item_category (
   id SMALLINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  subject_id TINYINT,
-  parent_id SMALLINT NULL,
-  code VARCHAR(10),
-  code_hierarchy VARCHAR(100), -- Looks like "1-1|1-LT|SOCK_R" for an ELA target, should we also store the reverse for easy hierarchy loading?
+  subject_id TINYINT NOT NULL,
+  parent_id SMALLINT,
+  code VARCHAR(10) NOT NULL,
+  code_hierarchy VARCHAR(100) NOT NULL, -- Looks like "1-1|1-LT|SOCK_R" for an ELA target, should we also store the reverse for easy hierarchy loading?
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   CONSTRAINT fk__item_category__parent FOREIGN KEY (parent_id) REFERENCES item_category(id) ON DELETE CASCADE,
   CONSTRAINT fk__item_category__subject FOREIGN KEY (subject_id) REFERENCES subject(id),
@@ -44,8 +44,8 @@ CREATE TABLE item_category (
 
 -- Create sub-score table for holding category (claim and target) scores.
 CREATE TABLE exam_sub_scores (
-  exam_id BIGINT,
-  category_id SMALLINT,
+  exam_id BIGINT NOT NULL,
+  category_id SMALLINT NOT NULL,
   scale_score FLOAT,
   scale_score_std_err FLOAT,
   performance_level TINYINT,
@@ -63,11 +63,11 @@ CREATE TABLE exam_sub_scores (
 
 -- Add category references to assessment items
 ALTER TABLE item
-  ADD COLUMN category_id SMALLINT NULL, -- This should reference the target (leaf) category
+  ADD COLUMN category_id SMALLINT, -- This should reference the target (leaf) category
   ADD CONSTRAINT fk__item__item_category FOREIGN KEY (category_id) REFERENCES item_category(id);
 
 ALTER TABLE item_other_target
-  ADD COLUMN category_id SMALLINT NULL,
+  ADD COLUMN category_id SMALLINT,
   ADD CONSTRAINT fk__item_other_target__item_category FOREIGN KEY (category_id) REFERENCES item_category(id);
 
 -- TODO after Ingest completion:
