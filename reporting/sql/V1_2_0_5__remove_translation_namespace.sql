3fa6d69110c2e710763d9441d24014d66f80994d
wtritch <wtritchler@fairwaytech.com>
2018-05-02 14:23:15 -0700

    Rename translation table to accommodation_translation to denote that it only contains accommodation translations.

--- reporting/sql/V1_2_0_5__remove_translation_namespace.sql
+++ reporting/sql/V1_2_0_5__remove_translation_namespace.sql
@@ -1,5 +1,7 @@
 -- Remove the `namespace` column from the translation table since we no longer
 -- require or respect it in the reporting application.
+-- Rename the existing reporting translation table to accommodation_translation
+-- to denote that it only contains accommodation translation labels
 
 use ${schemaName};
 
@@ -8,6 +10,11 @@ ALTER TABLE translation
   ADD PRIMARY KEY(language_code, label_code),
   DROP COLUMN namespace;
 
+-- Rename translation table to accommodation_translation
+RENAME TABLE translation TO accommodation_translation;
+
 -- Remove the `namespace` column from the translation staging table.
 ALTER TABLE staging_translation
   DROP COLUMN namespace;
+
+RENAME TABLE staging_translation TO accommodation_staging_translation;
