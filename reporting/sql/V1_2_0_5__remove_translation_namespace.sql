5c9ab7353d03ca0acb7c27eb34a0cfd9d00aa223
wtritch <wtritchler@fairwaytech.com>
2018-05-02 12:57:04 -0700

    Remove reporting.translation.namespace column since it is no longer required or respected.

---
+++ reporting/sql/V1_2_0_5__remove_translation_namespace.sql
@@ -0,0 +1,24 @@
+-- Remove the `namespace` column from the translation table since we no longer
+-- require or respect it in the reporting application.
+
+use ${schemaName};
+
+-- Create the new translation table
+CREATE TABLE new_translation (
+  label_code VARCHAR(128),
+  language_code VARCHAR(3),
+  label text,
+  PRIMARY KEY (language_code, label_code)
+);
+
+-- Migrate existing translations into new table
+INSERT INTO new_translation (label_code, language_code, label)
+  SELECT
+    label_code,
+    language_code,
+    label
+  FROM translation;
+
+DROP TABLE translation;
+
+RENAME TABLE new_translation TO translation;
