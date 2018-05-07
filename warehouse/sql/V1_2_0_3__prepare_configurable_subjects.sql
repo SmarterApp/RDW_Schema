1f5e19d821a17143bcd01e93feb821286b028e50
wtritch <wtritchler@fairwaytech.com>
2018-05-07 10:39:42 -0700

    Refactor configurable subject schema to account for using claims and targets rather than categories.

--- warehouse/sql/V1_2_0_3__prepare_configurable_subjects.sql
+++ warehouse/sql/V1_2_0_3__prepare_configurable_subjects.sql
@@ -49,60 +49,44 @@ CREATE TABLE subject_asmt_type (
   sub_score_performance_level_count TINYINT,
   sub_score_performance_level_standard_cutoff TINYINT,
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY(asmt_type_id, subject_id),
   CONSTRAINT fk__subject_asmt_type__asmt_type FOREIGN KEY (asmt_type_id) REFERENCES asmt_type(id),
   CONSTRAINT fk__subject_asmt_type__subject FOREIGN KEY (subject_id) REFERENCES subject(id)
 );
 
--- Table for holding hierarchical item categories (replaces claim and target tables)
-CREATE TABLE item_category (
-  id SMALLINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  subject_id TINYINT NOT NULL,
-  parent_id SMALLINT,
-  code VARCHAR(10) NOT NULL,
-  code_hierarchy VARCHAR(100) NOT NULL, -- Looks like "1-1|1-LT|SOCK_R" for an ELA target, should we also store the reverse for easy hierarchy loading?
-  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT fk__item_category__parent FOREIGN KEY (parent_id) REFERENCES item_category(id) ON DELETE CASCADE,
-  CONSTRAINT fk__item_category__subject FOREIGN KEY (subject_id) REFERENCES subject(id),
-  UNIQUE INDEX idx__item_category__code_hierarchy(code_hierarchy)
-);
-
--- Create sub-score table for holding category (claim and target) scores.
--- TODO alter exam_claim_score
-CREATE TABLE exam_sub_scores (
+-- Create exam_target_score table for holding category target scores.
+CREATE TABLE exam_target_score (
+  id BIGINT NOT NULL PRIMARY KEY,
   exam_id BIGINT NOT NULL,
-  category_id SMALLINT NOT NULL,
+  target_id SMALLINT NOT NULL,
   scale_score FLOAT,
   scale_score_std_err FLOAT,
-  performance_level TINYINT,
+  category TINYINT,
   theta_score FLOAT,
   theta_score_std_err FLOAT,
   residual_overall_score FLOAT,
   residual_overall_score_std_err FLOAT,
   residual_standard_met_score FLOAT,
   residual_standard_met_score_std_err FLOAT,
-  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY(exam_id, category_id),
-  CONSTRAINT fk__exam_sub_scores__exam FOREIGN KEY (exam_id) REFERENCES exam(id),
-  CONSTRAINT fk__exam_sub_scores__item_category FOREIGN KEY (category_id) REFERENCES item_category(id)
+  created timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  CONSTRAINT fk__exam_target_score__exam FOREIGN KEY (exam_id) REFERENCES exam(id),
+  CONSTRAINT fk__exam_target_score__target FOREIGN KEY (target_id) REFERENCES target(id)
 );
 
--- Add category references to assessment items
-ALTER TABLE item
-  ADD COLUMN category_id SMALLINT, -- This should reference the target (leaf) category
-  ADD CONSTRAINT fk__item__item_category FOREIGN KEY (category_id) REFERENCES item_category(id);
 
-ALTER TABLE item_other_target
-  ADD COLUMN category_id SMALLINT,
-  ADD CONSTRAINT fk__item_other_target__item_category FOREIGN KEY (category_id) REFERENCES item_category(id);
-
--- TODO after Ingest completion:
--- migrate existing item claim/target references to category references
--- migrate existing exam_claim_score rows to exam_sub_scores
+-- Insert data for Math: 1, ELA: 2
+-- ICA: 1, IAB: 2, SUM: 3
+INSERT INTO subject_asmt_type (asmt_type_id, subject_id, performance_level_count, performance_level_standard_cutoff, sub_score_performance_level_count, sub_score_performance_level_standard_cutoff) VALUES
+  (1, 1, 4, 3, 3, null),
+  (2, 1, 3, null, null, null),
+  (3, 1, 4, 3, 3, null),
+  (1, 2, 4, 3, 3, null),
+  (2, 2, 3, null, null, null),
+  (3, 2, 4, 3, 3, null);
 
 -- TODO after Feature completion:
+-- add constraints to subject table
 -- drop item.claim_id, item.target_id columns
--- drop exam_claim_score table
+-- drop unused columns from exam_claim_score table, add constraints
 -- drop descriptions from tables, display text should come from translation table
 -- drop claim, target tables
