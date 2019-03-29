5c2c44448f50a9ec0673be10000334ab6aaaaf6e
Mark Laffoon <laffoon@san.rr.com>
2019-03-29 12:55:55 -0700

    add validation of student group count, by school and by school year

---
+++ validation/sql/student-groups-by-year/reporting.sql
@@ -0,0 +1,4 @@
+SELECT
+    school_year, count(*) AS count
+  FROM student_group
+  GROUP BY school_year;
---
+++ validation/sql/student-groups-by-year/warehouse.sql
@@ -0,0 +1,5 @@
+SELECT
+  school_year, count(*) AS count
+FROM student_group
+WHERE deleted = 0
+GROUP BY school_year;
